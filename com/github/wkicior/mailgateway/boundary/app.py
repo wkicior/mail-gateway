f8c502819f3ead0862909172baabd86545fd6dc1
Wojciech Kicior <Wojciech.Kicior@gmail.com>
2015-03-15 22:03:10 +0100

    sample_input

--- com/github/wkicior/mailgateway/boundary/app.py
+++ com/github/wkicior/mailgateway/boundary/app.py
@@ -1,19 +1,25 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, redirect, url_for
 #from wwoproxy.service.service import WwoService
 import json
 
 app = Flask(__name__)
 
-@app.route("/mail-gateway/notification/")
-def forecast():
-    #wwo_service = WwoService()
-    #forecast_dic =  wwo_service.get_forecast(latitude, longitude)
-    res = json.dumps({"Status" : "OK"})
-    return Response(response=res, status=200, mimetype="application/json")
-
 @app.route("/")
 def index():
     return json.dumps({"status" : "OK"})
+
+
+@app.route("/mail-gateway/notification/", methods=['POST'])
+def forecast():
+    #wwo_service = WwoService()
+    #forecast_dic =  wwo_service.get_forecast(latitude, longitude)
+    #res = json.dumps(request.form['plan'])
+    res = request.get_json()
+    print res
+    print "OK"
+    return redirect(url_for('index'))
+
+
 
 if __name__ == "__main__":
     app.run()
