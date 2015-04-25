2de93673c71fe33183578af3ed6e76ea188d0e7f
Wojciech Kicior <Wojciech.Kicior@gmail.com>
2015-04-25 18:34:37 +0200

    generating messages with forecast rating

--- com/github/wkicior/mailgateway/tests/test.py
+++ com/github/wkicior/mailgateway/tests/test.py
@@ -4,7 +4,7 @@ import tempfile
 import mox
 import json
 from com.github.wkicior.mailgateway.model.mail import Mail
-from com.github.wkicior.mailgateway.model.notification import Notification
+from com.github.wkicior.mailgateway.model.notification import Notification, Rating
 from com.github.wkicior.mailgateway.service.mailservice import MailService
 from com.github.wkicior.mailgateway.service.smtpproxy import SmtpProxy
 
@@ -18,10 +18,12 @@ class TestParser(unittest.TestCase):
 
 
     def test_parse(self):
-        self.smtp_proxy.send_mail(Mail("user@localhost", "msg", "Forecast", "Helyeah@gmail.com"))
+        self.smtp_proxy.send_mail(Mail("user@localhost", "msg\nPROMISING starting from 12-12-2010 12:00", "Forecast", "Helyeah@gmail.com"))
         self.smtp_proxy_mocker.ReplayAll()
-        self.mail_service.send_mail(Notification("user@localhost", "msg"))
+        
+        self.mail_service.send_mail(Notification("user@localhost", "msg", Rating("PROMISING", "2010-12-12T12:00:00.000Z")))
         self.smtp_proxy_mocker.VerifyAll()
+
 
 if __name__ == '__main__':
     unittest.main()
